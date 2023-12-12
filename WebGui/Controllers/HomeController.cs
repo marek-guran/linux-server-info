58053adcf4fbf3872b50495859f02f296f067019
Marek Guráň <skemgi2@gmail.com>
2023-12-12 11:21:59 +0100

    Added simple loading of json

--- WebGui/Controllers/HomeController.cs
+++ WebGui/Controllers/HomeController.cs
@@ -1,22 +1,36 @@
 using Linux_Server_Info.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
 
 namespace Linux_Server_Info.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment; // Add this field
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment) // Inject IWebHostEnvironment
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment; // Assign the injected web host environment to the field
         }
 
         public IActionResult Index()
         {
-            return View();
+            var jsonFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "api/system_info.json");
+
+            using (var streamReader = new StreamReader(jsonFilePath))
+            {
+                var json = streamReader.ReadToEnd();
+                var serverInfo = JsonSerializer.Deserialize<ServerInfoModel>(json);
+                return View(serverInfo); // Pass the deserialized object to the view
+            }
         }
+
 
         public IActionResult Privacy()
         {
